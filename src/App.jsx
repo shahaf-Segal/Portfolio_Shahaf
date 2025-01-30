2ae2641a2bb39a6d97ddf39fbd1996095b4e0fed
shahaf-Segal <shahaf100@outlook.co.il>
2025-01-30 23:47:42 +0200

    started from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import SideBar from "./components/SideBar/SideBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <SideBar />
       <Routes>
         <Route path="/" element={<div></div>} />
         <Route path="/projects" element={<div></div>} />
---
+++ src/components/Atoms/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+  text: string;
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
+  onClick(): any;
+  type: "primary" | "secondary" | "borderless" | "outline";
+  fullWidth?: boolean;
+  backgroundColor: "dark" | "light";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  icon,
+  iconPosition = "left",
+  onClick,
+  type = "primary",
+  fullWidth = false,
+  backgroundColor = "light",
+}) => {
+  const buttonClassName = `${styles.button} ${styles[`button-${type}`]} ${
+    styles[`button-${type}-${backgroundColor}`]
+  }`;
+
+  return (
+    <button
+      className={buttonClassName}
+      style={{ width: fullWidth ? "100%" : "fit-content" }}
+      onClick={onClick}
+    >
+      {icon && iconPosition === "right" && icon}
+      {text}
+      {icon && iconPosition === "left" && icon}
+    </button>
+  );
+};
+
+export default Button;
