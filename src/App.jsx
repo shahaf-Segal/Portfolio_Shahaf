604314ced75bcbe9eaa44bc7151a59848abc135c
shahaf-Segal <shahaf100@outlook.co.il>
2025-01-31 02:01:44 +0200

    added scroll behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
 import About from "./components/Sections/About/About.jsx";
+import Home from "./components/Sections/Home/Home.jsx";
 import NavBar from "./components/Sections/NavBar/NavBar.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <About />
-    </BrowserRouter>
+    <main className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Home />
+        <About />
+      </BrowserRouter>
+    </main>
   );
 }
 
