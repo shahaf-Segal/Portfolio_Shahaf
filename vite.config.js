97e5a150b5835ac01da7d1534ac2e795ef34f58e
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-03 23:24:26 +0200

    new alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@atoms": "/src/components/Atoms",
+      "@contexts": "/src/Context",
+    },
+  },
   plugins: [react()],
-})
+});
