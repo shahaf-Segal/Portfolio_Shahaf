8b0e67983d685eedd112fee408647a863b8a4a70
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-04 04:37:56 +0200

    change color and font

--- src/components/Sections/NavBar/NavBar.jsx
+++ src/components/Sections/NavBar/NavBar.jsx
@@ -8,27 +8,37 @@ const NavBar = () => {
   return (
     <div className={Styles.NavBar}>
       <div className={Styles.LogoContainer}>
-        {/* Add your logo here */}
-        {/* <img src="logo.png" alt="Logo" /> */}
-      </div>
-      <div className={Styles.NavLinks}>
-        <Link to="#App">Home</Link>
-        <Link to="#about">About</Link>
-        <Link to="#projects">Projects</Link>
-      </div>
-      <div className={Styles.SocialsContainer}>
-        <ThemeButton />
-        {/* Add your social links here */}
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} />
+        <a href="#App">
+          SH
+          <span>{"{"}</span>S<span>{"}"}</span>
         </a>
-        <a
-          href="https://linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
+      </div>
+
+      <div className={Styles.RightSide}>
+        <div className={Styles.SocialsContainer}>
+          <ThemeButton />
+          {/* Add your social links here */}
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+        <div className={Styles.NavLinks}>
+          <Link to="#App">About</Link>
+          <Link to="#projects">Projects</Link>
+          <Link to="#skills">Skills</Link>
+          <Link to="#contact">Contact</Link>
+        </div>
       </div>
     </div>
   );
