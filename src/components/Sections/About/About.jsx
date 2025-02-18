deee987a784cdb97a6205a6088bbc1d1c95bd473
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-18 19:24:49 +0200

    added skills

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -1,6 +1,31 @@
+import {
+  faCss3,
+  faHtml5,
+  faJs,
+  faNode,
+  faPhp,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SkillContainer from "../../Molecules/SkillContainer/SkillContainer";
 import Styles from "./About.module.css";
+
 function About() {
-  return <div className={Styles["section_about"]} id="about"></div>;
+  const skills = [
+    { icon: <FontAwesomeIcon icon={faJs} />, name: "JavaScript" },
+    { icon: <FontAwesomeIcon icon={faJs} />, name: "TypeScript" },
+    { icon: <FontAwesomeIcon icon={faHtml5} />, name: "HTML" },
+    { icon: <FontAwesomeIcon icon={faCss3} />, name: "CSS" },
+    { icon: <FontAwesomeIcon icon={faNode} />, name: "Node.js" },
+    { icon: <FontAwesomeIcon icon={faPhp} />, name: "PHP" },
+    { icon: <FontAwesomeIcon icon={faDatabase} />, name: "MongoDB" },
+  ];
+
+  return (
+    <div className={Styles["section_about"]} id="about">
+      <SkillContainer skills={skills} />
+    </div>
+  );
 }
 
 export default About;
