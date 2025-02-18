dc84fdbe4f6de00e5efb626b286793e747479d75
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-18 23:05:28 +0200

    added title and moved skills out of component

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -1,29 +1,54 @@
 import {
   faCss3,
+  faGithub,
   faHtml5,
+  faJava,
   faJs,
+  faLaravel,
   faNode,
   faPhp,
+  faPython,
+  faReact,
 } from "@fortawesome/free-brands-svg-icons";
-import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { faDatabase, faToolbox } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SkillContainer from "../../Molecules/SkillContainer/SkillContainer";
-import Styles from "./About.module.css";
+import Styles from "./About.module.scss";
+
+const SkillSet = [
+  { icon: <FontAwesomeIcon icon={faReact} />, name: "React" },
+  { icon: <FontAwesomeIcon icon={faJs} />, name: "JavaScript" },
+  { icon: <FontAwesomeIcon icon={faJs} />, name: "TypeScript" },
+  { icon: <FontAwesomeIcon icon={faHtml5} />, name: "HTML" },
+  { icon: <FontAwesomeIcon icon={faCss3} />, name: "CSS" },
+  { icon: <FontAwesomeIcon icon={faNode} />, name: "Node.js" },
+  { icon: <FontAwesomeIcon icon={faPhp} />, name: "PHP" },
+  { icon: <FontAwesomeIcon icon={faDatabase} />, name: "MongoDB" },
+];
+
+const SkillTools = [
+  { icon: <FontAwesomeIcon icon={faGithub} />, name: "GitHub" },
+  { icon: <FontAwesomeIcon icon={faPython} />, name: "Python" },
+  { icon: <FontAwesomeIcon icon={faJava} />, name: "Java" },
+  { icon: <FontAwesomeIcon icon={faToolbox} />, name: "Assembly" },
+  { icon: <FontAwesomeIcon icon={faLaravel} />, name: "Laravel" },
+];
 
 function About() {
-  const skills = [
-    { icon: <FontAwesomeIcon icon={faJs} />, name: "JavaScript" },
-    { icon: <FontAwesomeIcon icon={faJs} />, name: "TypeScript" },
-    { icon: <FontAwesomeIcon icon={faHtml5} />, name: "HTML" },
-    { icon: <FontAwesomeIcon icon={faCss3} />, name: "CSS" },
-    { icon: <FontAwesomeIcon icon={faNode} />, name: "Node.js" },
-    { icon: <FontAwesomeIcon icon={faPhp} />, name: "PHP" },
-    { icon: <FontAwesomeIcon icon={faDatabase} />, name: "MongoDB" },
-  ];
-
   return (
     <div className={Styles["section_about"]} id="about">
-      <SkillContainer skills={skills} />
+      <div className={Styles.skillSection}>
+        <h1>
+          My <span className="highlight_text">Skillset</span>
+        </h1>
+        <SkillContainer skills={SkillSet} />
+      </div>
+      <div className={Styles.skillSection}>
+        <h1>
+          <span className="highlight_text">Tools</span> I know
+        </h1>
+        <SkillContainer skills={SkillTools} />
+      </div>
     </div>
   );
 }
