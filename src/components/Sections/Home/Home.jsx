ec76812999d5b3e7e3b4b1e2b7d4b92bb4852b1b
shahaf-Segal <shahaf100@outlook.co.il>
2025-03-03 14:05:12 +0200

    intro section changes

--- src/components/Sections/Home/Home.jsx
+++ src/components/Sections/Home/Home.jsx
@@ -1,15 +1,19 @@
 import Styles from "./Home.module.scss";
+import SelfIntroduction from "./SelfIntroduction/SelfIntroduction";
 
 import Hero from "./Hero";
 function Home() {
   return (
     <div className={Styles.section_home}>
-      <Hero />
-      <img
-        src="/images/demo_profilepic.png"
-        alt="Profile Picture"
-        className={Styles.profile_picture}
-      />
+      <div className={Styles.top_section}>
+        <Hero />
+        <img
+          src="/images/demo_profilepic.png"
+          alt="Profile Picture"
+          className={Styles.profile_picture}
+        />
+      </div>
+      <SelfIntroduction />
     </div>
   );
 }
