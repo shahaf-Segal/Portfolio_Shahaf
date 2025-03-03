9b97e34351af9a8b2240bec3ad8818bdebc77c85
shahaf-Segal <shahaf100@outlook.co.il>
2025-03-03 18:00:33 +0200

    modify self project

--- src/components/Sections/Home/SelfIntroduction/SelfIntroduction.jsx
+++ src/components/Sections/Home/SelfIntroduction/SelfIntroduction.jsx
@@ -6,14 +6,15 @@ function SelfIntroduction() {
     <div className={Styles.selfIntroduction}>
       <h1>Get To Know Me</h1>
       <p>
-        Hi, I'm <span className={Styles.highlight_text}>Shahaf Segal</span>.
-        <br /> A{" "}
         <span className={Styles.highlight_text}>Full-Stack developer</span>{" "}
-        specializing in React,Node.js and PHP .
+        specializing in React,Node.js and PHP.
         <br />I create consistent, efficient, and innovative solutions to
         complex problems.
+        <br />
+        I'm a quick learner, a team player, and a problem solver.
+        <br /> Always learning new technologies and improving my skills.
       </p>
-      <p>When I'm not coding, I enjoy</p>
+      <p className={Styles.highlight_text}>When I'm not coding, I enjoy</p>
       <ul>
         <li>Playing video games</li>
         <li>Learning Japanese</li>
