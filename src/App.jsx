df9b7ead7c5b1afb35e7e64adaa7c52c2d9660ce
shahaf-Segal <shahaf100@outlook.co.il>
2025-07-08 02:56:48 +0300

    changed pathing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { ThemeContext } from "@contexts/ThemeProvider";
 import { useContext } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Footnote from "./components/Sections/footnote/Footnote.jsx";
 import Home from "./components/Sections/Home/Home.jsx";
 import NavBar from "./components/Sections/NavBar/NavBar.jsx";
 import Projects from "./components/Sections/Projects/Projects.jsx";
+import SingleProject from "./components/Sections/SingleProject/SingleProject.jsx";
 import SkillSection from "./components/Sections/Skills/SkillSection.jsx";
-
 function App() {
   const { IsDark } = useContext(ThemeContext);
 
@@ -16,9 +16,20 @@ function App() {
       <BrowserRouter>
         <div id="top"></div>
         <NavBar />
-        <Home />
-        <SkillSection />
-        <Projects />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <Home />
+                <SkillSection />
+                <Projects />
+              </>
+            }
+          />
+          <Route path="/project/:projectId" element={<SingleProject />} />
+        </Routes>
         <Footnote />
       </BrowserRouter>
     </main>
